c0f562604b9fb9b6495d90a664e5ec0615318a64
IWW Tech <tech+github@iww.org>
2021-08-16 12:32:39 -0400

    fixed path to static assets for heroku

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
