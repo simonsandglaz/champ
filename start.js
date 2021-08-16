c97a998b4da3b15115ece57ffd775890ac54a22a
IWW Tech <tech+github@iww.org>
2021-08-16 12:36:58 -0400

    fixed port for heroku

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 
-const server = app.listen(5000, () => {
-  console.log(`Express is running on port ${server.address().port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
